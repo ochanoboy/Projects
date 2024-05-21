3c3824930b8abd563363317bc4cd00ce9420957f
Kirill <120396317+ochanoboy@users.noreply.github.com>
2024-05-21 17:56:09 +0500

    Update translator.py

--- translator/translator.py
+++ translator/translator.py
@@ -2,10 +2,10 @@ from translate import Translator
 
 translator= Translator(to_lang="ja")
 try:
-  with open('GitHub/Python/Python-exercises/translator/text.txt', mode='r') as my_file:
+  with open('./text.txt', mode='r') as my_file:
     txt = my_file.read()
     translation = (translator.translate(txt))
-    with open('GitHub/Python/Python-exercises/translator/test-ja.txt', mode='w') as my_file2:
+    with open('./test-ja.txt', mode='w') as my_file2:
       my_file2.write(translation)
 except FileNotFoundError as e:
   print('check your file path silly!')
